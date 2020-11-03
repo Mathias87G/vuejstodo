84435f420a98c4d674d949aa0b12ef076512b921
Mathias <mathias.grandin@gmail.com>
2020-11-03 16:59:29 +0100

    Complete

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.app')
+@section('content')
+    <div class="w-100 h-100 d-flex flex-column justify-content-center align-items-center">
+        <h1 class="text-white display-4 mb-5">Vue Js Todo APP</h1>
+        <todo-component></todo-component>
+    </div>
+@endsection
